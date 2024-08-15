94b11e86dd50d09c7e832145c3a8ea87f7fc5915
DavidIvan025 <davidivansan@hotmail.com>
2024-08-14 22:03:32 -0600

    feat: add html file and js function prototype method

--- JS/test.js
+++ JS/test.js
@@ -1,13 +1,37 @@
-/* Function.prototype.method = function (name, func) {
-    this.prototype[name] = func;
-    return this;
-}; */
+// Add a method conditionally.
+Function.prototype.method = function (name, func) {
+    if (!this.prototype[name]) {
+        this.prototype[name] = func;
+        return this;
+    }
+};
 
-function test() {
-    var testVar = document.getElementById("test");
-    var newDiv = document.createElement("a");
-    newDiv.innerHTML = "Click Here!"
-    testVar.appendChild(newDiv)
-}
+var Quo = function (string) {
+    this.status = string;
+};
+// called get_status.
+Quo.prototype.get_statu = function () {
+    return this.status;
+};
+// Make an instance of Quo.
+var myQuo = new Quo("confused");
+document.writeln(myQuo.get_statu());
 
-test()
+
+var statusObject = {
+    status: 'A-OK'
+};
+
+// statusObject does not inherit from Quo.prototype,
+// but we can invoke the get_status method on
+// statusObject even though statusObject does not have
+// a get_status method.
+var status = Quo.prototype.get_statu.apply(statusObject);
+
+document.writeln(status)
+
+
+Number.method('integer', function () {
+    return Math[this < 0 ? 'ceil' : 'floor'](this);
+});
+document.writeln((-10 / 3).integer());
